51f891712d0c0245af91874be43c37532bf29daf
Ethan Law and Katy Henning <ethan@>
2016-11-03 16:08:48 -0700

    add some styles

--- app/services/shopping-cart.js
+++ app/services/shopping-cart.js
@@ -10,25 +10,19 @@ export default Ember.Service.extend({
     }
     item.set('quantity', item.get('quantity') + 1);
   },
-
   remove(item) {
-    if(item.get('quantity') === 1){
+    if(item.get('quantity') === 1) {
     this.get('items').removeObject(item);
     }
     item.set('quantity', item.get('quantity') -1);
   },
-
   empty() {
     this.get('items').clear();
   },
-
   totalCart(price) {
     this.set('total', price + this.get('total'));
   },
-
   removeFromTotal(price) {
     this.set('total', this.get('total') - price);
   }
-
-
 });
